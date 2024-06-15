1788d295623af05c42f603c8b71f6659a0d61c4f
Galib Mia <gm71bd@gmail.com>
2024-06-15 07:53:57 +0600

    Left side nav bar designed and load data from api

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+            .catch(err => console.log(err.message))
+    }, []);
+
+
     return (
         <div>
-            <h1>Left Nav</h1>
+            <h4 className='ps-2 mb-3'>All Category</h4>
+            <ListGroup className=''>
+                {
+                    categories.map(category => <Link className='text-decoration-none p-2'><ListGroup.Item className='bg-custom-dark border-none mb-1 h5 text-secondary' key={category.id}>{category.name}</ListGroup.Item></Link>)
+                }
+            </ListGroup>
         </div>
     );
 };
